55c9beb67cea8695422b64ef22c08bb1ee7f9e28
Enzo <enzo.rva@gmail.com>
2023-08-16 18:27:17 -0300

    Code opmization and 2 crypto displays added

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,8 +1,10 @@
 import 'package:bitcoin_ticker/services/prices.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/cupertino.dart';
 import 'dart:io' show Platform;
 import 'coin_data.dart';
+import 'package:bitcoin_ticker/widgets/crypto_card_widget.dart';
 
 class PriceScreen extends StatefulWidget {
   const PriceScreen({super.key});
@@ -12,14 +14,26 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = 'USD';
   final getdata = PriceModel();
+  final numberFormat = NumberFormat("#,##0.00", "en_US");
+
+  String selectedCurrency = 'USD';
   double price = 0.0;
+  String roundedBtcPrice = "?";
+  String roundedEthPrice = "?";
+  String roundedLtcPrice = "?";
 
   @override
   void initState() {
     super.initState();
     //init();
+  }
+
+  Future<String> safePrice(String selectedCurrency, String cryptoName) async {
+    final ltcMap = await getdata.getCryptoPrice(selectedCurrency, cryptoName);
+    double ltcPrice = ltcMap['rate'];
+    String roundedLtcPrice = numberFormat.format(ltcPrice);
+    return roundedLtcPrice;
   }
 
   DropdownButton<String> androidDropdown() {
@@ -36,11 +50,15 @@ class _PriceScreenState extends State<PriceScreen> {
       value: selectedCurrency,
       items: dropdownItems,
       onChanged: (value) async {
-        final item = await getdata.getBtcPrice(value!);
-        price = item['rate'];
-        setState(() {
-          selectedCurrency = value!;
-        });
+        selectedCurrency = value ?? "USD";
+
+        roundedBtcPrice = await safePrice(selectedCurrency, 'BTC');
+
+        roundedEthPrice = await safePrice(selectedCurrency, 'ETH');
+
+        roundedLtcPrice = await safePrice(selectedCurrency, 'LTC');
+
+        setState(() {});
       },
     );
   }
@@ -84,29 +102,28 @@ class _PriceScreenState extends State<PriceScreen> {
         children: <Widget>[
           Padding(
             padding: const EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = $price USD',
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                CryptoCardWidget(
+                    cryptoName: 'BTC',
+                    roundedPrice: roundedBtcPrice,
+                    selectedCurrency: selectedCurrency),
+                CryptoCardWidget(
+                    cryptoName: 'ETH',
+                    roundedPrice: roundedEthPrice,
+                    selectedCurrency: selectedCurrency),
+                CryptoCardWidget(
+                    cryptoName: 'LTC',
+                    roundedPrice: roundedLtcPrice,
+                    selectedCurrency: selectedCurrency),
+              ],
             ),
           ),
-          ElevatedButton(
-              onPressed: () => getdata.getBtcPrice(selectedCurrency),
-              child: const Text('test')),
+
+          // ElevatedButton(
+          //     onPressed: () => getdata.getBtcPrice(selectedCurrency),
+          //     child: const Text('test')),
           Container(
               height: 150.0,
               alignment: Alignment.center,
